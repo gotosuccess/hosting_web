096ee6ebe55ffc4c526f7e82044767819f1d3303
gotosuccess <101387956+gotosuccess@users.noreply.github.com>
2022-03-12 20:03:47 +0300

    Add files via upload

---
+++ Hosting-Company/js/main.js
@@ -0,0 +1,84 @@
+class ItcTabs {
+  constructor(target, config) {
+    const defaultConfig = {};
+    this._config = Object.assign(defaultConfig, config);
+    this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
+    this._elButtons = this._elTabs.querySelectorAll('.tabs__btn');
+    this._elPanes = this._elTabs.querySelectorAll('.tabs__pane');
+    this._eventShow = new Event('tab.itc.change');
+    this._init();
+    this._events();
+  }
+  _init() {
+    this._elTabs.setAttribute('role', 'tablist');
+    this._elButtons.forEach((el, index) => {
+      el.dataset.index = index;
+      el.setAttribute('role', 'tab');
+      this._elPanes[index].setAttribute('role', 'tabpanel');
+    });
+  }
+  show(elLinkTarget) {
+    const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+    const elLinkActive = this._elTabs.querySelector('.tabs__btn_active');
+    const elPaneShow = this._elTabs.querySelector('.tabs__pane_show');
+    if (elLinkTarget === elLinkActive) {
+      return;
+    }
+    elLinkActive ? elLinkActive.classList.remove('tabs__btn_active') : null;
+    elPaneShow ? elPaneShow.classList.remove('tabs__pane_show') : null;
+    elLinkTarget.classList.add('tabs__btn_active');
+    elPaneTarget.classList.add('tabs__pane_show');
+    this._elTabs.dispatchEvent(this._eventShow);
+    elLinkTarget.focus();
+  }
+  showByIndex(index) {
+    const elLinkTarget = this._elButtons[index];
+    elLinkTarget ? this.show(elLinkTarget) : null;
+  };
+  _events() {
+    this._elTabs.addEventListener('click', (e) => {
+      const target = e.target.closest('.tabs__btn');
+      if (target) {
+        e.preventDefault();
+        this.show(target);
+      }
+    });
+  }
+}
+
+new ItcTabs('.tabs');
+
+
+// <!-- jQuery -->
+
+$(function () {
+  $('#switch-btn-1').click(function () {
+    $(this).toggleClass('switch-on');
+    if ($(this).hasClass('switch-on')) {
+      $(this).trigger('on.switch');
+    } else {
+      $(this).trigger('off.switch');
+    }
+  });
+  $('#switch-btn-1').on('on.switch', function () {
+    $('#block-1').addClass('bl-hide');
+    $('#block-2').removeClass('bl-hide');
+  });
+  $('.switch-btn').on('off.switch', function () {
+    $('#block-1').removeClass('bl-hide');
+    $('#block-2').addClass('bl-hide');
+  });
+});
+
+
+$('.js-tab-trigger').click(function() {
+  // клик! 
+  var id = $(this).attr('data-tab'), // 1
+       content = $('.js-tab-content[data-tab="'+ id +'"]'); // 2
+       
+       $('.js-tab-trigger.active').removeClass('active'); // 1
+   $(this).addClass('active'); // 2
+   
+   $('.js-tab-content.active').removeClass('active'); // 3
+   content.addClass('active'); // 4
+})
